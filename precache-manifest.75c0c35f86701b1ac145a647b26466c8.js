7e7b0190a55b4e3fb228d326fb819b6a06d44cbe
bea_lazo <beatamayo90@gmail.com>
2022-07-04 11:29:45 +0200

    Updates

---
+++ precache-manifest.75c0c35f86701b1ac145a647b26466c8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5665c78f07c1bda30ad",
+    "url": "/todo-list/static/css/main.e9cdd775.chunk.css"
+  },
+  {
+    "revision": "e5665c78f07c1bda30ad",
+    "url": "/todo-list/static/js/main.e5665c78.chunk.js"
+  },
+  {
+    "revision": "ee8408d5f64d712378a8",
+    "url": "/todo-list/static/js/1.ee8408d5.chunk.js"
+  },
+  {
+    "revision": "6010a0f7acc162bd8c5a",
+    "url": "/todo-list/static/js/2.6010a0f7.chunk.js"
+  },
+  {
+    "revision": "35789bd0057de26d106e",
+    "url": "/todo-list/static/js/3.35789bd0.chunk.js"
+  },
+  {
+    "revision": "d937204f4f73c6f10795",
+    "url": "/todo-list/static/js/4.d937204f.chunk.js"
+  },
+  {
+    "revision": "3bcd1bbac86daace08f6",
+    "url": "/todo-list/static/js/5.3bcd1bba.chunk.js"
+  },
+  {
+    "revision": "3f68dc8e757585101207",
+    "url": "/todo-list/static/js/runtime~main.3f68dc8e.js"
+  },
+  {
+    "revision": "5cfb7be323dd8131caf367877a115281",
+    "url": "/todo-list/static/media/mujer.5cfb7be3.png"
+  },
+  {
+    "revision": "a1164054ced7f3c43e05a4a2025b600c",
+    "url": "/todo-list/static/media/banner.a1164054.jpg"
+  },
+  {
+    "revision": "366e76931641f43ea7a5510202467ac1",
+    "url": "/todo-list/index.html"
+  }
+];
